7859d3be8a5163b9fa3ea681fadafb7fd3966f46
Kinuax <kinuax@gmail.com>
2018-02-21 13:42:02 +0100

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ontzia',
-    version='0.1.0',
+    version='0.1.1',
     packages=['ontzia'],
     install_requires=['flask', 'gunicorn'],
     setup_requires=['pytest-runner'],
