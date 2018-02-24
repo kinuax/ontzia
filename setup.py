d4c846ef9ebe1aa51bf44dc8a50b5c913f6d9ec3
Kinuax <kinuax@gmail.com>
2018-02-24 14:35:41 +0100

    Add ics related requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='ontzia',
     version='0.1.1',
     packages=['ontzia'],
-    install_requires=['flask', 'flask-restful', 'gunicorn'],
+    install_requires=['flask', 'flask-restful', 'gunicorn', 'icalendar', 'pytz'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
 )
