d6a8eb1f8b80c37b3bc3c2a432f46ca07cf77f85
Kinuax <kinuax@gmail.com>
2018-02-24 18:01:52 +0100

    Add ics file uploading support

--- ontzia/views.py
+++ ontzia/views.py
@@ -1,6 +1,19 @@
+from flask import render_template, request
+
 from ontzia import app
 
 
 @app.route('/')
 def index():
     return 'Showing index'
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+
+        if file.content_type != 'text/calendar':
+            return render_template('upload.html', error='invalid file')
+
+    return render_template('upload.html')
